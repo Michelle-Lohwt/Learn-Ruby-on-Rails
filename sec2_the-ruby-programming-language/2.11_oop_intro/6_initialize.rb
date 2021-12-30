b83644348963976d46521b1d50703855bccfcaef
michelle <michellelohwt0799@gmail.com>
2021-12-30 16:08:48 +0800

    fix: move initialize as first method in the class

--- sec2_the-ruby-programming-language/2.11_oop_intro/6_initialize.rb
+++ sec2_the-ruby-programming-language/2.11_oop_intro/6_initialize.rb
@@ -3,17 +3,17 @@
 class Student
   attr_accessor :first_name, :last_name, :email, :username, :password
 
-  def to_s
-    "First name: #{@first_name}, Last name: #{@last_name}, Username: #{@username}, 
-    Email: #{@email}, Password: #{@password}"
-  end
-
   def initialize(fname, lname, uname, eml, pwd)
     @first_name = fname
     @last_name = lname
     @username = uname
     @email = eml
     @password = pwd
+  end
+
+  def to_s
+    "First name: #{@first_name}, Last name: #{@last_name}, Username: #{@username}, 
+    Email: #{@email}, Password: #{@password}"
   end
 
 end
