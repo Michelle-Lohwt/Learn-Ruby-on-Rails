3b8e6eb969ef811e650f6852af2cb27f7acc1749
michelle <michellelohwt0799@gmail.com>
2021-12-31 12:48:30 +0800

    fix: add spacings

--- sec2_the-ruby-programming-language/2.11_oop_intro/recap.rb
+++ sec2_the-ruby-programming-language/2.11_oop_intro/recap.rb
@@ -13,8 +13,9 @@ class User
   def self.identify_yourself
     puts "Hey I am a class method"
   end
-  
+
 end
+
 user = User.new("mashrur", "mashrur@example.com")
 user.run
 User.identify_youself # to run this class method you don't need an instance of user 
