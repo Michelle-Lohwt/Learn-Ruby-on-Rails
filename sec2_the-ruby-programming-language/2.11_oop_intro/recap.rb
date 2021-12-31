fcdc5fc669cbe46edf58c009c0e5132d5cdf4abf
michelle <michellelohwt0799@gmail.com>
2021-12-31 12:47:28 +0800

    fix: add spacings

--- sec2_the-ruby-programming-language/2.11_oop_intro/recap.rb
+++ sec2_the-ruby-programming-language/2.11_oop_intro/recap.rb
@@ -5,12 +5,15 @@ class User
     @name = name
     @email = email
   end
+
   def run
     puts "Hey I'm running"
   end
+
   def self.identify_yourself
     puts "Hey I am a class method"
   end
+  
 end
 user = User.new("mashrur", "mashrur@example.com")
 user.run
