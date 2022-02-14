f5009befe31151caa3fe59706d6198f4985640c7
michelle <michellelohwt0799@gmail.com>
2022-02-14 16:31:01 +0800

    fix: indentation

--- sec2_the-ruby-programming-language/2.12_final_ruby_project/1_bcrypt.rb
+++ sec2_the-ruby-programming-language/2.12_final_ruby_project/1_bcrypt.rb
@@ -11,8 +11,6 @@ puts my_password == "not my password" #=> false
 my_password = BCrypt::Password.new("$2a$12$j/iC4gfOgmExeLyK7PHYA.Ahxpks7P3rUlxr.DmSKIqUFgu3h8RPu")
 puts my_password == "my password"     #=> true
 
-
-
 my_password_1 = BCrypt::Password.create("my password")
 my_password_2 = BCrypt::Password.create("my password")
 
